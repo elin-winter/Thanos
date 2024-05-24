74bb273effd95bb13b3de2202c48cd8da0a2fcf6
elin-winter <elinlwinter1@fmail.com>
2024-05-24 20:17:56 -0300

    Parcial Terminado

--- README.md
+++ README.md
@@ -11,12 +11,18 @@ Saber la energía total de un universo que es la sumatoria de todas las energía
 
 Segunda parte
 A su vez, aunque el guantelete no se encuentre completo con las 6 gemas, el poseedor puede utilizar el poder del mismo contra un enemigo, es decir que puede aplicar el poder de cada gema sobre el enemigo. Las gemas del infinito fueron originalmente parte de la entidad primordial llamada Némesis, un ser todopoderoso del universo anterior quién prefirió terminar su existencia en lugar de vivir como la única conciencia en el universo. Al morir, dio paso al universo actual, y el núcleo de su ser reencarnó en las seis gemas: 
-La mente que tiene la habilidad de debilitar la energía de un usuario en un valor dado.
-El alma puede controlar el alma de nuestro oponente permitiéndole eliminar una habilidad en particular si es que la posee. Además le quita 10 puntos de energía. 
-El espacio que permite transportar al rival al planeta x (el que usted decida) y resta 20 puntos de energía.
-El poder deja sin energía al rival y si tiene 2 habilidades o menos se las quita (en caso contrario no le saca ninguna habilidad).
-El tiempo que reduce a la mitad la edad de su oponente pero como no está permitido pelear con menores, no puede dejar la edad del oponente con menos de 18 años. Considerar la mitad entera, por ej: si el oponente tiene 50 años, le quedarán 25. Si tiene 45, le quedarán 22 (por división entera). Si tiene 30 años, le deben quedar 18 en lugar de 15. También resta 50 puntos de energía.
-La gema loca que permite manipular el poder de una gema y la ejecuta 2 veces contra un rival.
+
+- La mente que tiene la habilidad de debilitar la energía de un usuario en un valor dado.
+  
+- El alma puede controlar el alma de nuestro oponente permitiéndole eliminar una habilidad en particular si es que la posee. Además le quita 10 puntos de energía. 
+  
+- El espacio que permite transportar al rival al planeta x (el que usted decida) y resta 20 puntos de energía.
+  
+- El poder deja sin energía al rival y si tiene 2 habilidades o menos se las quita (en caso contrario no le saca ninguna habilidad).
+  
+- El tiempo que reduce a la mitad la edad de su oponente pero como no está permitido pelear con menores, no puede dejar la edad del oponente con menos de 18 años. Considerar la mitad entera, por ej: si el oponente tiene 50 años, le quedarán 25. Si tiene 45, le quedarán 22 (por división entera). Si tiene 30 años, le deben quedar 18 en lugar de 15. También resta 50 puntos de energía.
+  
+- La gema loca que permite manipular el poder de una gema y la ejecuta 2 veces contra un rival.
 
 Punto 3: (3 puntos) Implementar las gemas del infinito, evitando lógica duplicada. 
 
--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,143 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+-- -------------------- Dominio -----------------------
+
+data Guantelete = UnGuantelete{
+    material :: Material, 
+    gemas :: [Gema]
+}
+
+data Persona = UnaPersona{
+    edad :: Number,
+    energia :: Energia,
+    habilidades :: [Habilidad],
+    nombre :: Nombre,
+    planeta :: Planeta
+}
+
+-- -------------------- Defnición de Tipos -----------------------
+type Material = String
+type Habilidad = String
+type Nombre = String
+type Planeta = String
+type Energia = Number
+
+type Universo = [Persona]
+type Gema = Persona -> Persona
+
+-- -------------------- Funciones -------------------------
+-- ----------------- Primera Parte
+-- Punto 1
+chasquidoUniverso :: Universo -> Guantelete -> Universo
+chasquidoUniverso universo guante
+    | estaCompleto guante =  eliminarMitad universo
+    | otherwise = universo
+
+eliminarMitad :: Universo -> Universo
+eliminarMitad universo = drop (cantVictimas universo) universo
+
+estaCompleto :: Guantelete -> Bool
+estaCompleto guante = length (gemas guante) == 6 && material guante == "Uru"
+
+cantVictimas :: Universo -> Number
+cantVictimas universo = div (length universo) 2
+
+--Punto 2
+aptoPendex :: Universo -> Bool
+aptoPendex universo = any ((<45) . edad) universo
+
+energiaTotalUniverso :: Universo -> Energia
+energiaTotalUniverso = sum . (map energia) . filter ((>1). length . habilidades)
+
+-- ----------------- Segunda Parte
+--Punto 3
+aplicarPoderGema :: Gema -> Persona -> Persona
+aplicarPoderGema gema enemigo = gema enemigo
+
+-- Funcion 1
+mente :: Number -> Gema
+mente = debilitarEnergia
+
+debilitarEnergia :: Number -> Persona -> Persona
+debilitarEnergia delta enemigo = enemigo { energia = energia enemigo - delta}
+
+-- Funcion 2
+alma :: Habilidad -> Number -> Gema
+alma habilidad delta = debilitarEnergia delta . quitarHabilidad habilidad
+
+quitarHabilidad :: Habilidad -> Persona -> Persona
+quitarHabilidad habilidad enemigo
+    | elem habilidad (habilidades enemigo) = enemigo { habilidades = habilidadesSin habilidad enemigo}
+    | otherwise = enemigo
+
+habilidadesSin :: Habilidad -> Persona -> [Habilidad]
+habilidadesSin habilidad enemigo = filter (/= habilidad) (habilidades enemigo)
+
+-- Funcion 3
+espacio :: Planeta -> Gema
+espacio planetaX = debilitarEnergia 20 . transportarEnemigo planetaX
+
+transportarEnemigo :: Planeta -> Persona -> Persona
+transportarEnemigo planetaX enemigo = enemigo { planeta = planetaX}
+
+-- Funcion 4
+poder :: Gema
+poder enemigo = ((debilitarEnergia (energia enemigo)) . quitarHabilidadSi) enemigo
+
+quitarHabilidadSi :: Persona -> Persona
+quitarHabilidadSi enemigo
+    | length (habilidades enemigo) <= 2 = enemigo { habilidades = []}
+    | otherwise = enemigo
+
+-- Funcion 5
+tiempo :: Gema
+tiempo = (debilitarEnergia 50) . reducirEdad
+
+reducirEdad :: Persona -> Persona
+reducirEdad enemigo = enemigo{ edad = max 18 (div (edad enemigo) 2)}
+
+-- Funcion 2
+loca :: Gema -> Gema
+loca gema = aplicarPoderGema gema . aplicarPoderGema gema
+
+--Punto 4
+guante :: Guantelete
+guante = UnGuantelete "Goma" [tiempo, alma "Usar Mjolnir" 10, loca (alma "Programacion en Haskell" 4)]
+
+--Punto 5
+aplicarGemas :: [Gema] -> Gema
+aplicarGemas = foldr (.) id
+
+-- Punto 6
+gemaMasPoderosa :: Guantelete -> Persona -> Gema
+gemaMasPoderosa guante = evaluarPoderosa (gemas guante) 
+
+evaluarPoderosa :: [Gema] -> Persona -> Gema
+evaluarPoderosa [x] _ = x
+evaluarPoderosa (gema1:gema2:gemas) enemigo 
+    | mayorEnergia gema1 gema2 enemigo = evaluarPoderosa (gema1:gemas) enemigo
+    | otherwise = evaluarPoderosa (gema2:gemas) enemigo
+
+mayorEnergia :: Gema -> Gema -> Persona -> Bool
+mayorEnergia gema1 gema2 enemigo = (energia . gema1) enemigo > (energia . gema2) enemigo
+
+-- Punto 7
+{-
+
+gemaMasPoderosa punisher guanteleteDeLocos
+
+Esta funcion no podría terminar de ejecutarse nunca, ya que al utilizar recursividad
+su implementación requiere llegar al final de la cola (que no existe), por lo que
+la máquina se quedaría infinitamente evaluando las gemas. 
+
+usoLasTresPrimerasGemas guanteleteDeLocos punisher
+
+Esta función si podría evaluarse ya que, aunque la lista es infinita, no se la requiere completa
+Haskell al tener un motor de evaluacion diferida (lazy) primero va a entrar en la función, no en los 
+parametros, se va a dar cuenta que solo necesita 3 y los va a tomar. Nunca va a analizar toda la estructura
+porque no necesita hacerlo. 
+
+-}
+
+
